79d5362f834f51f5e2b59b0e808d15bfdb683061
JRemkes <jeffreyremkes@hotmail.com>
2017-03-31 09:37:28 +0200

    Main css and default page

--- TFDH/view/tpls/bezoeker/default.php
+++ TFDH/view/tpls/bezoeker/default.php
@@ -1,8 +1,61 @@
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Default page</title>
+        <title>Training factory</title>
+
+        <!-- CSS -->
+        <link href="TFDH/css/main.css" rel="stylesheet">
+
+        <!-- JS -->
     </head>
     <body>
-        <h1>Default page</h1>
+
+        <div id="body-wrapper">
+
+            <header>
+                <div id="title">
+                    <img src="TFDH/img/logo.png">
+                    <h1>Training centrum Den Haag</h1>
+                </div>
+                <div id="login-wrapper">
+                    <form>
+                        <input type="text" name="username" placeholder="username">
+                        <input type="password" name="password" placeholder="wachtwoord">
+                        <button>reset ww</button>
+                        <input type="submit" value="inloggen">
+                    </form>
+                </div>
+                <div class="clear"></div>
+            </header>
+
+            <nav>
+                <ul>
+                    <li class="active"><a>Home</a></li>
+                    <li><a>Trainings Aanbod</a></li>
+                    <li><a>Lid worden</a></li>
+                    <li><a>Gedragsregels</a></li>
+                    <li><a>Lokatie & contact</a></li>
+                </ul>
+            </nav>
+
+            <div id="content-wrapper">
+                <section id="photo-col">
+                    <figure>
+                        <img src="TFDH/img/boksen_1.jpg">
+                    </figure>
+                    <figure>
+                        <img src="TFDH/img/boksen_2.jpg">
+                    </figure>
+                    <figure>
+                        <img src="TFDH/img/boksen_3.png">
+                    </figure>
+                    <div class="clear"></div>
+                </section>
+
+                <section id="main-content">
+                    <h1>Home</h1>
+                </section>
+            </div>
+        </div>
     </body>
 </html>
